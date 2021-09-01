92643d379e405b6a7ce3537638d719a66534d021
Stefan Probst <stefanprobst@posteo.de>
2021-09-01 11:18:56 +0200

    fix: handle empty image in video card

--- src/cms/components/VideoCard.tsx
+++ src/cms/components/VideoCard.tsx
@@ -12,7 +12,7 @@ import type { FilePath } from '@/utils/ts/aliases'
 export interface VideoCardProps extends Omit<VideoProps, 'caption'> {
   title: string
   subtitle: string
-  image: FilePath | { src: FilePath; width: number; height: number }
+  image?: FilePath | { src: FilePath; width: number; height: number }
 }
 
 /**
@@ -28,16 +28,18 @@ export function VideoCard(props: VideoCardProps): JSX.Element {
         className="flex flex-col items-center w-full p-12 space-y-4 transition rounded shadow-lg text-neutral-800 hover:shadow-xl focus:outline-none focus-visible:ring focus-visible:ring-primary-600"
       >
         <div className="w-full">
-          <Image
-            src={props.image}
-            alt=""
-            className="!my-0"
-            layout="responsive"
-            width="16"
-            height="9"
-            objectFit="cover"
-            sizes="640px"
-          />
+          {props.image != null ? (
+            <Image
+              src={props.image}
+              alt=""
+              className="!my-0"
+              layout="responsive"
+              width="16"
+              height="9"
+              objectFit="cover"
+              sizes="640px"
+            />
+          ) : null}
         </div>
         <Icon icon={PlayIcon} className="w-16 h-16 text-primary-600" />
         <strong className="text-2xl font-bold">{props.title}</strong>
