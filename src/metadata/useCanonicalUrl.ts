9e285321d018dc71745535608a9ac6af69144aa9
Stefan Probst <stefanprobst@kolabnow.com>
2021-11-19 11:35:04 +0100

    fix: don't add locale prefix twice to canonical url

--- src/metadata/useCanonicalUrl.ts
+++ src/metadata/useCanonicalUrl.ts
@@ -3,8 +3,8 @@ import { useRouter } from 'next/router'
 import { useMemo } from 'react'
 
 import { useLocale } from '@/i18n/useLocale'
-import { useSiteMetadata } from '@/metadata/useSiteMetadata'
 import { createUrl } from '@/utils/createUrl'
+import { url as baseUrl } from '~/config/site.config'
 
 /**
  * Returns the canonical URL for the current route's pathname.
@@ -15,19 +15,18 @@ import { createUrl } from '@/utils/createUrl'
 export function useCanonicalUrl(query?: NextRouter['query']): string {
   const router = useRouter()
   const { locale } = useLocale()
-  const { url: siteUrl } = useSiteMetadata()
 
   const canonicalUrl = useMemo(() => {
     const { pathname } = createUrl({ pathname: router.asPath })
     const url = createUrl({
-      baseUrl: siteUrl,
+      baseUrl,
       locale,
-      pathname: pathname,
+      pathname,
       query,
     })
 
     return String(url)
-  }, [router, locale, siteUrl, query])
+  }, [router, locale, query])
 
   return canonicalUrl
 }
