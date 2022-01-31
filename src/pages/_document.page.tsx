80d346515d22f9ab8f169e5bd3455c89d4b0134a
Stefan Probst <stefanprobst@posteo.de>
2022-01-31 11:24:03 +0100

    fix: ensure italic fonts

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -15,7 +15,7 @@ export default class NextDocument extends Document {
             crossOrigin="anonymous"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
           />
         </Head>
