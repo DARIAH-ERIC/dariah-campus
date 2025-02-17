4a37e715cc801dc0df132468a407778c516234db
Stefan Probst <stefanprobst@posteo.de>
2025-02-17 15:47:07 +0100

    fix: correctly render text quiz

--- components/content/quiz-form.tsx
+++ components/content/quiz-form.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { AlertCircleIcon, CheckIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { AlertCircleIcon, CheckIcon } from "lucide-react";
 import { type ReactNode, useActionState, useTransition } from "react";
-import { Button as AriaButton, type ButtonProps as AriaButtonProps } from "react-aria-components";
 
 import { useQuizContext } from "@/components/content/quiz";
+import { QuizControls } from "@/components/content/quiz-controls";
 
 export interface QuizFormState {
-	status: "correct" | "incorrect";
+	status: "initial" | "correct" | "incorrect";
 }
 
 interface QuizFormProps {
@@ -31,7 +31,7 @@ export function QuizForm(props: Readonly<QuizFormProps>): ReactNode {
 		validateButtonLabel,
 	} = props;
 
-	const { isCurrent, navigation } = useQuizContext();
+	const { isCurrent } = useQuizContext();
 	const [formState, formAction] = useActionState(validate, undefined);
 	const [_isPending, startTransition] = useTransition();
 
@@ -54,21 +54,11 @@ export function QuizForm(props: Readonly<QuizFormProps>): ReactNode {
 				{children}
 
 				<footer>
-					<div className="flex items-center justify-between gap-x-4">
-						<Button isDisabled={!navigation.hasPrevious} onPress={navigation.previous}>
-							<ChevronLeftIcon aria-hidden={true} className="size-4 shrink-0" />
-							<span>{previousButtonLabel}</span>
-						</Button>
-
-						<Button type="submit">
-							<span>{validateButtonLabel}</span>
-						</Button>
-
-						<Button isDisabled={!navigation.hasNext} onPress={navigation.next}>
-							<span>{nextButtonLabel}</span>
-							<ChevronRightIcon aria-hidden={true} className="size-4 shrink-0" />
-						</Button>
-					</div>
+					<QuizControls
+						nextButtonLabel={nextButtonLabel}
+						previousButtonLabel={previousButtonLabel}
+						validateButtonLabel={validateButtonLabel}
+					/>
 
 					<div
 						aria-live="polite"
@@ -94,22 +84,5 @@ export function QuizForm(props: Readonly<QuizFormProps>): ReactNode {
 				</footer>
 			</form>
 		</section>
-	);
-}
-
-interface ButtonProps extends AriaButtonProps {
-	children: ReactNode;
-}
-
-function Button(props: ButtonProps): ReactNode {
-	const { children } = props;
-
-	return (
-		<AriaButton
-			{...props}
-			className="inline-flex cursor-default items-center justify-center gap-x-2 whitespace-nowrap rounded-md px-3 py-1.5 text-sm font-medium leading-normal transition hover:bg-neutral-100 pressed:bg-neutral-200 disabled:opacity-50"
-		>
-			{children}
-		</AriaButton>
 	);
 }
--- components/content/quiz-text-input.tsx
+++ components/content/quiz-text-input.tsx
@@ -1,6 +1,8 @@
 import { useTranslations } from "next-intl";
 import type { ReactNode } from "react";
 
+import { QuizForm, type QuizFormState } from "@/components/content/quiz-form";
+
 interface QuizTextInputProps {
 	children: ReactNode;
 }
@@ -8,15 +10,33 @@ interface QuizTextInputProps {
 export function QuizTextInput(props: Readonly<QuizTextInputProps>): ReactNode {
 	const { children } = props;
 
-	const t = useTranslations("content.QuizTextInput");
+	const t = useTranslations("content.QuizControls");
+
+	// eslint-disable-next-line @typescript-eslint/require-await
+	async function validate(_state: QuizFormState | undefined, _formData: FormData) {
+		"use server";
+
+		return { status: "initial" as const };
+	}
 
 	return (
-		<div>
-			<header>{children}</header>
-			<label>
-				<span className="sr-only">{t("label")}</span>
-				<input type="text" />
-			</label>
-		</div>
+		<QuizForm
+			errorMessages={[]}
+			nextButtonLabel={t("next-question")}
+			previousButtonLabel={t("previous-question")}
+			successMessages={[]}
+			validate={validate}
+			validateButtonLabel={t("validate")}
+		>
+			<div className="mb-6">
+				{children}
+
+				<label>
+					{/* eslint-disable-next-line react/jsx-no-literals */}
+					<span className="sr-only">Input</span>
+					<input className="w-full rounded-md border border-neutral-300 px-4 py-2" />
+				</label>
+			</div>
+		</QuizForm>
 	);
 }
