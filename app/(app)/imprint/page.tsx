4f5ef53fa8dfdcbadb3fa5cdaede4e71c741ae6c
Stefan Probst <stefanprobst@posteo.de>
2025-02-26 14:45:10 +0100

    fix: use correct imprint text

--- app/(app)/imprint/page.tsx
+++ app/(app)/imprint/page.tsx
@@ -1,13 +1,14 @@
-import { HttpError, request } from "@acdh-oeaw/lib";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+
 import type { Metadata, ResolvingMetadata } from "next";
-import { notFound } from "next/navigation";
 import { getLocale, getTranslations } from "next-intl/server";
 import type { ReactNode } from "react";
 
 import { MainContent } from "@/components/main-content";
 import { PageTitle } from "@/components/page-title";
-import type { Locale } from "@/config/i18n.config";
-import { createImprintUrl } from "@/config/imprint.config";
+import { compileMdx } from "@/lib/content/compile-mdx";
+import { getLanguage } from "@/lib/i18n/get-language";
 
 interface ImprintPageProps extends EmptyObject {}
 
@@ -30,29 +31,22 @@ export default async function ImprintPage(_props: Readonly<ImprintPageProps>): P
 	const locale = await getLocale();
 	const t = await getTranslations("ImprintPage");
 
-	const html = await getImprintHtml(locale);
+	const filePath = join(process.cwd(), "app", "(app)", "imprint", "legal-notice.mdx");
+	const fileContent = await readFile(filePath, "utf-8");
+	const { default: Content } = await compileMdx(
+		fileContent,
+		getLanguage(locale),
+		new URL(import.meta.url),
+	);
 
 	return (
 		<MainContent className="mx-auto grid w-full max-w-screen-lg content-start gap-y-24 px-4 py-8 xs:px-8 xs:py-16 md:py-24">
 			<div className="grid gap-y-4">
 				<PageTitle>{t("title")}</PageTitle>
 			</div>
-			<div dangerouslySetInnerHTML={{ __html: html }} className="prose" />
+			<div className="prose">
+				<Content />
+			</div>
 		</MainContent>
 	);
-}
-
-async function getImprintHtml(locale: Locale): Promise<string> {
-	try {
-		const url = createImprintUrl(locale);
-		const html = await request(url, { responseType: "text" });
-
-		return html;
-	} catch (error) {
-		if (error instanceof HttpError && error.response.status === 404) {
-			notFound();
-		}
-
-		throw error;
-	}
 }
