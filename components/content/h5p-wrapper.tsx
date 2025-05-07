683794bb2b5e6a946393714a0aa166daf9b81f00
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2025-05-07 14:19:54 +0000

    refactor: preload h5p

--- components/content/h5p-wrapper.tsx
+++ components/content/h5p-wrapper.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Script from "next/script";
-import { type ReactNode, useEffect, useRef, useState } from "react";
+import { type ReactNode, useEffect, useRef } from "react";
 
 interface H5PWrapperProps {
 	path: string;
@@ -17,7 +17,10 @@ type H5PConstructor = new (
 		id?: string;
 		preventH5PInit?: boolean;
 	},
-) => Promise<string>;
+) => {
+	destroy?: () => void;
+	[key: string]: unknown;
+};
 
 interface H5PModule {
 	H5P: H5PConstructor;
@@ -25,20 +28,20 @@ interface H5PModule {
 
 declare global {
 	interface Window {
-		H5PStandalone: H5PModule;
+		H5PStandalone?: H5PModule;
 	}
 }
 
 export function H5PWrapper(props: H5PWrapperProps): ReactNode {
 	const { path } = props;
-	const [h5pReady, setH5pReady] = useState(false);
 	const ref = useRef<HTMLDivElement | null>(null);
 
 	useEffect(() => {
 		const container = ref.current;
 
-		if (!h5pReady || !container) return;
-		const loadH5P = async () => {
+		if (!window.H5PStandalone?.H5P || !container) return;
+		const { H5P } = window.H5PStandalone;
+		const loadH5P = () => {
 			const options = {
 				h5pJsonPath: `/vendor/h5p-content/${path}`,
 				librariesPath: "/vendor/h5p-shared-libraries",
@@ -46,26 +49,23 @@ export function H5PWrapper(props: H5PWrapperProps): ReactNode {
 				frameCss: "/vendor/h5p-standalone/styles/h5p.css",
 			};
 			try {
-				await new window.H5PStandalone.H5P(container, options);
+				new H5P(container, options);
 			} catch (error: unknown) {
 				console.error("Error loading H5P content", error);
 			}
 		};
-		void loadH5P();
+		loadH5P();
 		return () => {
 			container.innerHTML = "";
 		};
-	}, [path, h5pReady]);
+	}, [path]);
 	return (
 		<>
 			<div ref={ref} />
 			<Script
 				id="h5p-script"
-				onLoad={() => {
-					setH5pReady(true);
-				}}
 				src="/vendor/h5p-standalone/main.bundle.js"
-				strategy="lazyOnload"
+				strategy="beforeInteractive"
 			></Script>
 		</>
 	);
