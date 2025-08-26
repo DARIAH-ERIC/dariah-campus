05827f7ff0da314c51c98b53e91239cfd7982f21
Stefan Probst <stefanprobst@posteo.de>
2025-08-26 19:01:39 +0200

    fix: make rss feed static

--- app/(app)/rss.xml/route.ts
+++ app/(app)/rss.xml/route.ts
@@ -2,6 +2,8 @@ import { type NextRequest, NextResponse } from "next/server";
 
 import { createFeed } from "@/lib/rss/create-feed";
 
+export const dynamic = "force-static";
+
 export async function GET(_request: NextRequest): Promise<NextResponse> {
 	const feed = await createFeed();
 
