7a44bb483543cd4f56a79967aa3c0d0b59748601
Stefan Probst <stefanprobst@posteo.de>
2025-08-29 20:24:34 +0200

    build: patch mermaid, fix turbopack

--- next.config.ts
+++ next.config.ts
@@ -75,16 +75,6 @@ const config: Config = {
 			config.plugins.push(localesPlugin.webpack({ locales: [] }));
 		}
 
-		/**
-		 * Avoid bundling `playwright`, which is needed by `rehype-mermaid`.
-		 *
-		 * @see https://github.com/microsoft/playwright/issues/33031
-		 */
-		if (isServer) {
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-			config.externals.push("playwright-core");
-		}
-
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-return
 		return config;
 	},
