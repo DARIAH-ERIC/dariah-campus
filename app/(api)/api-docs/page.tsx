fad7b3d2c5d8ab710cd7a8af88dc5ce482681dfb
Stefan Probst <stefanprobst@posteo.de>
2025-09-24 20:56:57 +0200

    feat: add api docs

---
+++ app/(api)/api-docs/page.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+
+import { ApiDocs } from "@/app/(api)/api-docs/_components/api-docs";
+
+export default function ApiDocsPage(): ReactNode {
+	return <ApiDocs />;
+}
