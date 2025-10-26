ccbc55f2deb56ea2d368dcf60d6bccff7d068460
Stefan Probst <stefanprobst@posteo.de>
2025-10-26 16:50:40 +0100

    chore: move mdx types to global types

--- lib/content/types.ts
+++ lib/content/types.ts
@@ -1,18 +1,8 @@
-import type * as runtime from "react/jsx-runtime";
-
 import type { components } from "@/lib/content/mdx/components";
 import type { IntlLanguage } from "@/lib/i18n/locales";
 
 declare global {
 	type MDXProvidedComponents = typeof components;
-}
-
-declare module "mdx/types" {
-	namespace JSX {
-		type Element = runtime.JSX.Element;
-		type ElementClass = runtime.JSX.ElementClass;
-		type IntrinsicElements = runtime.JSX.IntrinsicElements;
-	}
 }
 
 declare module "@acdh-oeaw/mdx-lib" {
