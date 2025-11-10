457ac97f461a3f0459d3d1b5f258955e2a4be6b8
stefanprobst <stefanprobst@posteo.de>
2025-11-10 07:18:16 +0100

    chore: cleanup (#1628)

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,6 +1,9 @@
+/* eslint-disable no-restricted-syntax */
+
 import { join } from "node:path";
 
-import { defineConfig, devices } from "@playwright/test";
+import { isNonEmptyString } from "@acdh-oeaw/lib";
+import { defineConfig, devices, type PlaywrightTestConfig } from "@playwright/test";
 import { config as dotenv } from "dotenv";
 import { expand } from "dotenv-expand";
 import isCI from "is-in-ci";
@@ -13,9 +16,34 @@ for (const envFilePath of [".env.test.local", ".env.local", ".env.test", ".env"]
 	expand(dotenv({ path: join(process.cwd(), envFilePath), quiet: true }));
 }
 
-// eslint-disable-next-line no-restricted-syntax
-const port = Number(process.env.PORT) || 3000;
-const baseUrl = `http://localhost:${String(port)}`;
+function getConfig():
+	| { kind: "remote"; baseUrl: string; webServer: undefined }
+	| { kind: "local"; baseUrl: string; webServer: PlaywrightTestConfig["webServer"] } {
+	const remoteBaseUrl = process.env.PLAYWRIGHT_TEST_APP_BASE_URL;
+
+	if (isNonEmptyString(remoteBaseUrl)) {
+		return {
+			kind: "remote",
+			baseUrl: remoteBaseUrl,
+			webServer: undefined,
+		};
+	}
+
+	const port = Number(process.env.PORT) || 3000;
+	const baseUrl = `http://localhost:${String(port)}`;
+
+	return {
+		kind: "local",
+		baseUrl,
+		webServer: {
+			command: `pnpm run start --port ${String(port)}`,
+			url: baseUrl,
+			reuseExistingServer: !isCI,
+		},
+	};
+}
+
+const config = getConfig();
 
 export default defineConfig({
 	testDir: "../e2e",
@@ -27,56 +55,41 @@ export default defineConfig({
 	workers: isCI ? 1 : undefined,
 	reporter: isCI ? [["github"], ["html", { open: "never" }]] : [["html"]],
 	use: {
-		baseURL: baseUrl,
+		baseURL: config.baseUrl,
 		screenshot: "on-first-failure",
 		trace: "on-first-retry",
 	},
 	projects: [
 		{
-			name: "setup",
-			testMatch: "global.setup.ts",
-		},
-		{
 			name: "chromium",
 			use: { ...devices["Desktop Chrome"], channel: "chromium" },
-			dependencies: ["setup"],
 		},
 		{
 			name: "firefox",
 			use: { ...devices["Desktop Firefox"] },
-			dependencies: ["setup"],
 		},
 		{
 			name: "webkit",
 			use: { ...devices["Desktop Safari"] },
-			dependencies: ["setup"],
 		},
 		/** Test against mobile viewports. */
 		// {
-		// 	name: "Mobile Chrome",
-		// 	use: { ...devices["Pixel 5"] },
-		// 	dependencies: ["setup"],
+		//     name: "Mobile Chrome",
+		//     use: { ...devices["Pixel 5"] },
 		// },
 		// {
-		// 	name: "Mobile Safari",
-		// 	use: { ...devices["iPhone 12"] },
-		// 	dependencies: ["setup"],
+		//     name: "Mobile Safari",
+		//     use: { ...devices["iPhone 12"] },
 		// },
 		/** Test against branded browsers. */
 		// {
-		// 	name: "Microsoft Edge",
-		// 	use: { ...devices["Desktop Edge"], channel: "msedge" },
-		// 	dependencies: ["setup"],
+		//     name: "Microsoft Edge",
+		//     use: { ...devices["Desktop Edge"], channel: "msedge" },
 		// },
 		// {
-		// 	name: "Google Chrome",
-		// 	use: { ...devices["Desktop Chrome"], channel: "chrome" },
-		// 	dependencies: ["setup"],
+		//     name: "Google Chrome",
+		//     use: { ...devices["Desktop Chrome"], channel: "chrome" },
 		// },
 	],
-	webServer: {
-		command: `pnpm run start --port ${String(port)}`,
-		url: baseUrl,
-		reuseExistingServer: !isCI,
-	},
+	webServer: config.webServer,
 });
