65d5f0cf2de15334b94ac077c673fd60c04e4401
Stefan Probst <stefanprobst@posteo.de>
2026-03-21 05:18:10 +0100

    chore: lint

--- components/page-title.tsx
+++ components/page-title.tsx
@@ -8,6 +8,6 @@ export function PageTitle(props: Readonly<PageTitleProps>): ReactNode {
 	const { children } = props;
 
 	return (
-		<h1 className="text-center text-4xl leading-10 font-extrabold text-balance">{children}</h1>
+		<h1 className="text-center text-4xl/10 font-extrabold text-balance">{children}</h1>
 	);
 }
