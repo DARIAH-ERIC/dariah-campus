ea668831b63ca197196a19aa2f26c73f673981f1
Stefan Probst <stefanprobst@posteo.de>
2026-03-24 14:55:03 +0100

    revert: revert updates from #1706 (#1710)
    this reverts the update from #1706.
    
    closes #1709

--- components/page-title.tsx
+++ components/page-title.tsx
@@ -7,5 +7,7 @@ interface PageTitleProps {
 export function PageTitle(props: Readonly<PageTitleProps>): ReactNode {
 	const { children } = props;
 
-	return <h1 className="text-center text-4xl/10 font-extrabold text-balance">{children}</h1>;
+	return (
+		<h1 className="text-center text-4xl leading-10 font-extrabold text-balance">{children}</h1>
+	);
 }
