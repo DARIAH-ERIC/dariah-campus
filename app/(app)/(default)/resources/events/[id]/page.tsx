6f5a62a3aec539446bbc5940c171cfc9a41fc9e5
Stefan Probst <stefanprobst@posteo.de>
2026-08-14 16:06:30 +0200

    style: adapt breakpoint for three-column layout

--- app/(app)/(default)/resources/events/[id]/page.tsx
+++ app/(app)/(default)/resources/events/[id]/page.tsx
@@ -226,9 +226,9 @@ export default async function EventResourcePage(
 				})}
 			/>
 
-			<div className="mx-auto grid w-full max-w-screen-lg gap-y-10 px-4 py-8 xs:px-8 xs:py-16 2xl:max-w-none 2xl:grid-cols-(--content-layout) 2xl:gap-x-10 2xl:gap-y-0">
+			<div className="mx-auto grid w-full max-w-screen-lg gap-y-10 px-4 py-8 xs:px-8 xs:py-16 xl:max-w-none xl:grid-cols-(--content-layout) xl:gap-x-8 xl:gap-y-0">
 				<aside
-					className="sticky top-24 hidden max-h-screen w-full max-w-xs gap-y-8 justify-self-end overflow-y-auto p-8 text-sm text-neutral-500 2xl:flex 2xl:flex-col"
+					className="sticky top-24 hidden max-h-screen w-full max-w-xs gap-y-8 justify-self-end overflow-y-auto p-6 text-sm text-neutral-500 xl:flex xl:flex-col 2xl:p-8"
 					style={{ maxHeight: "calc(100dvh - 12px - var(--page-header-height))" }}
 				>
 					<div className="flex flex-col gap-y-2 text-sm text-neutral-500">
@@ -387,7 +387,7 @@ export default async function EventResourcePage(
 							)}
 						</ol>
 					</Resource>
-					<div className="mx-auto mt-12 flex w-full max-w-(--size-content) flex-col gap-y-12 border-t border-neutral-200 pt-12 text-sm text-neutral-500 2xl:hidden">
+					<div className="mx-auto mt-12 flex w-full max-w-(--size-content) flex-col gap-y-12 border-t border-neutral-200 pt-12 text-sm text-neutral-500 xl:hidden">
 						<Citation
 							authors={await Promise.all(
 								authors.map(async (id) => {
@@ -468,7 +468,7 @@ export default async function EventResourcePage(
 				{tableOfContents.length > 0 ? (
 					<Fragment>
 						<aside
-							className="sticky top-24 hidden max-h-screen w-full max-w-xs overflow-y-auto p-8 text-sm text-neutral-500 2xl:flex 2xl:flex-col"
+							className="sticky top-24 hidden max-h-screen w-full max-w-xs overflow-y-auto p-6 text-sm text-neutral-500 xl:flex xl:flex-col 2xl:p-8"
 							style={{
 								maxHeight: "calc(100dvh - 12px - var(--page-header-height))",
 							}}
@@ -488,7 +488,7 @@ export default async function EventResourcePage(
 								}
 							/>
 						</aside>
-						<aside className="2xl:hidden">
+						<aside className="xl:hidden">
 							<FloatingTableOfContents
 								closeLabel={t("close")}
 								label={t("table-of-contents")}
